a8fecb11a4d4e7f3e7eb528c6dc30ea2584bbf05
sattorov <sattorov.ahu@gmail.com>
2024-09-14 20:32:55 +0500

    Bismillah

--- data/config.py
+++ data/config.py
@@ -5,9 +5,6 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = list(map(lambda pk: int(pk), env.list("ADMINS")))
-
-# NOTIFY_BOT_TOKEN = env.str("NOTIFY_BOT_TOKEN")
-# NOTIFY_CHANNEL_ID = env.str("NOTIFY_CHANNEL_ID")
 STATUS_IDS = {
     4: 'Опубликован',
     5: "Удалён со стороны заказчика",
